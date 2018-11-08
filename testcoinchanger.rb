4d277671f0bb55b478014efb0589d0decdbe42e9
tracydaniellelong <tracydaniellelong@gmail.com>
2018-11-07 19:23:50 -0500

    tests passing

--- testcoinchanger.rb
+++ testcoinchanger.rb
@@ -24,7 +24,7 @@ class Testing < Minitest::Test
 		assert_equal({:dime=>1, :nickel=>1, :penny=>4}, coin_changer(19))
 	end
 	def test_for_20_cents
-		assert_equal({}, coin_changer(20))
+		assert_equal({:dime=>2}, coin_changer(20))
 	end
 	def test_for_25_cents
 		assert_equal({:quarter=>1}, coin_changer(25))
@@ -45,13 +45,13 @@ class Testing < Minitest::Test
 		assert_equal({:quarter=>1, :dime=>1, :nickel=>1}, coin_changer(40))
 	end
 	def test_for_50_cents
-		assert_equal({}, coin_changer(50))
+		assert_equal({:quarter=>2}, coin_changer(50))
 	end
 	def test_for_51_cents
-		assert_equal({}, coin_changer(51))
+		assert_equal({:quarter=>2, :penny=>1}, coin_changer(51))
 	end
 	def test_for_60_cents
-		assert_equal({}, coin_changer(60))
+		assert_equal({:quarter=>2, :dime=>1}, coin_changer(60))
 	end
 
 end
